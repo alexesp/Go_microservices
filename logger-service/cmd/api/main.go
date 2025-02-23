05854d43e04d1b76c86d15f1b031990321c09db4
alexesp <aleizaes@hotmail.com>
2025-02-23 09:49:27 +0100

    crear mongodb conexion

--- logger-service/cmd/api/main.go
+++ logger-service/cmd/api/main.go
@@ -12,73 +12,71 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-
 const (
-	webPort = "80"
-	rpcPort = "5001"
-	mongoURL = "mongodb://mongo:27017"
+	webPort  = "80"
+	rpcPort  = "5001"
+	mongoURL = "mongodb://localhost:27017"
 	gRpcPort = "50001"
 )
 
 var client *mongo.Client
 
-type Config struct{
-Models data.Models
+type Config struct {
+	Models data.Models
 }
 
-func main(){
+func main() {
 	//conectar a mongodb
-mongoClient, err := connectToMongo()
+	mongoClient, err := connectToMongo()
 
-if err != nil{
-	log.Panic(err)
-}
-
-client = mongoClient
-
-//crear el contexto de disconeccion
-ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
-defer cancel()
-
-defer func (){
-	if err = client.Disconnect(ctx); err != nil{
-		panic(err)
+	if err != nil {
+		log.Panic(err)
 	}
-}()
 
-app := Config{
-	Models: data.New(client),
+	client = mongoClient
+
+	//crear el contexto de disconeccion
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	app := Config{
+		Models: data.New(client),
+	}
+
+	//start web server
+	go app.serve()
+
+	log.Println("Starting sevice on port", webPort)
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+	_ = srv.ListenAndServe()
+
+	// if err != nil{
+	// 	log.Panic()
+	// }
+
 }
 
-//start web server
-//go app.serve()
-log.Println("Starting sevice on port", webPort)
-srv := &http.Server{
-	Addr: fmt.Sprintf(":%s", webPort),
-	Handler: app.routes(),
-
-}
-_ = srv.ListenAndServe()
-
-// if err != nil{
-// 	log.Panic()
-// }
-
+func (app *Config) serve() {
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic()
+	}
 }
 
-// func (app *Config) serve(){
-// 	srv := &http.Server{
-// 		Addr: fmt.Sprintf(":%s", webPort),
-// 		Handler: app.routes(),
-
-// 	}
-// 	err := srv.ListenAndServe()
-// 	if err != nil{
-// 		log.Panic()
-// 	}
-// }
-
-func connectToMongo() (*mongo.Client, error){
+func connectToMongo() (*mongo.Client, error) {
 	clientOptions := options.Client().ApplyURI(mongoURL)
 
 	clientOptions.SetAuth(options.Credential{
@@ -87,7 +85,7 @@ func connectToMongo() (*mongo.Client, error){
 	})
 
 	c, err := mongo.Connect(context.TODO(), clientOptions)
-	if err != nil{
+	if err != nil {
 		log.Println("Error connecting: ", err)
 		return nil, err
 	}
